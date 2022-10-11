590e0958296cf459369ec719d976456da30a0f58
aliceauko <alice.auko@student.moringaschool.com>
2022-10-11 13:53:59 +0300

    Add a function to display the plant list on mylist component

--- client/src/components/MyList.js
+++ client/src/components/MyList.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import PlantList from './PlantList'
 
-function MyList() {
+function MyList({user, plants, onDeletePlant, onUpdatePlant}) {
+    
+  const displayPlants = !user? null : plants.filter((plant) => plant.user_id===user_id)
   return (
-    <div>
-      
-    </div>
+    !user? <h1>Please log in to see your plant list!</h1> :
+    <PlantList
+        plants={displayPlants} 
+        onDeletePlant={onDeletePlant}
+        onUpdatePlant={onUpdatePlant}
+        edit={true}
+    />
+  
   )
 }
 
