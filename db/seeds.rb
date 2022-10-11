a4439fd31fe26176c38c4778bddf32b494f980a8
aliceauko <alice.auko@student.moringaschool.com>
2022-10-11 11:00:38 +0300

    Seed your tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+types = Type.create([{ name: 'Flowering', description: 'Plants that produce flowers.' }, 
+                     { name: 'Low-light', description: 'Plants that do not require direct exposure to sunlight and can survive in low light conditions.' },
+                     { name: 'Air-purifying', description: 'Plants that help in cleaning air.' },
+                     { name: 'Trailing', description: 'Plants that have long twigs, branches or leaves that twirl other palnts.' },
+                     { name: 'Succulents and cacti', description: 'Plants that require little water to grow.' }])
+
