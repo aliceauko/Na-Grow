df3cf43017e0300f8176ed4aa0e5a825a3cf1f98
aliceauko <alice.auko@student.moringaschool.com>
2022-10-11 13:02:43 +0300

    Install react router dom and add the links on the navbar component

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import { NavLink, UseHistory } from "react-router-dom";
 
-function NavBar() {
+const styles = {
+  display: "inline-block",
+  width: "200px",
+  padding: "10px",
+  margin: "0 10px 10px",
+  background: "blue",
+  color: "white",
+  fontSize: "20px",
+};
+
+function NavBar({ setUser, user }) {
+  const history = UseHistory();
+
+  function handleLogout() {
+    fetch("/logout", {
+      method: "DELETE",
+    }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+        history.push("/");
+      }
+    });
+  }
+
   return (
     <div>
-      
+      <NavLink
+        to="/"
+        exact
+        style={styles}
+        activeStyle={{
+          background: "pink",
+          color: "black",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/new"
+        exact
+        style={styles}
+        activeStyle={{
+          background: "pink",
+          color: "black",
+        }}
+      >
+        Add New Plant
+      </NavLink>
+      <NavLink
+        to="/mylist"
+        exact
+        style={styles}
+        activeStyle={{
+          background: "pink",
+          color: "black",
+        }}
+      >
+        My List
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={styles}
+        activeStyle={{
+          background: "pink",
+          color: "black",
+        }}
+      >
+        {user ? `Hi! ${user.username}` : "Login"}
+      </NavLink>
+      <button onClick={handleLogout}>Logout</button>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
