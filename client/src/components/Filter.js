c388c38e3fb6065683945d4d87b6a4405429cd16
aliceauko <alice.auko@student.moringaschool.com>
2022-10-11 13:45:32 +0300

    Add the tyeps to filter from in the filter component

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function Filter() {
+function Filter({ types, onChangeId }) {
+  function handleFilter(e) {
+    onChangeId(types.filter((type) => type.name === e.target.value)[0].id);
+  }
   return (
     <div>
-      
+      <label>Choose a plant type: </label>
+      <select name="type_id" onChange={handleFilter}>
+        <option value="Flowering">Flowering</option>
+        <option value="Low-light">Low-light</option>
+        <option value="Air-purifying">Air-purifying</option>
+        <option value="Trailing">Trailing</option>
+        <option value="Succulents and cacti">Succulents and cacti</option>
+      </select>
     </div>
-  )
+  );
 }
 
-export default Filter
+export default Filter;
