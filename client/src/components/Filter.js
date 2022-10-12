60327a1d7412dfa1a97ad62c6d56c64a6d2d5203
aliceauko <alice.auko@student.moringaschool.com>
2022-10-12 14:27:18 +0300

    Change structure in filter component

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 function Filter({ types, onChangeId }) {
+
   function handleFilter(e) {
-    onChangeId(types.filter((type) => type.name === e.target.value)[0].id);
+    onChangeId(types.filter(type=>type.name===(e.target.value))[0].id);
   }
   return (
     <div>
       <label>Choose a plant type: </label>
       <select name="type_id" onChange={handleFilter}>
+        
         <option value="Flowering">Flowering</option>
         <option value="Low-light">Low-light</option>
         <option value="Air-purifying">Air-purifying</option>
