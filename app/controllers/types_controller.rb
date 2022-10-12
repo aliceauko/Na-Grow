3b902096fe07b34f21d3f756ed19e6d6b8828eff
aliceauko <alice.auko@student.moringaschool.com>
2022-10-12 15:39:06 +0300

    Uncomment the Types controller

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,13 +1,13 @@
-# class TypesController < ApplicationController
+class TypesController < ApplicationController
 
-#     def create
-#         type = Type.create(params)
-#         render json: type, status: :created
-#     end
+    def create
+        type = Type.create(params)
+        render json: type, status: :created
+    end
 
-#     def index
-#         types = Type.all
-#         render json: types
-#     end
+    def index
+        types = Type.all
+        render json: types
+    end
 
-# end
+end
