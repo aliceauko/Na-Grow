f47a5ef52aa56c6b8e98668a2aa02527c1684b69
aliceauko <alice.auko@student.moringaschool.com>
2022-10-14 08:36:21 +0300

    Done

--- client/src/components/NewPlant.js
+++ client/src/components/NewPlant.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom'
-// import Filter from './Filter';
+import Filter from './Filter';
 
 const style = {
     backgroundImage: 
@@ -12,14 +12,14 @@ const style = {
 
 
 
-function NewPlant({user, plants, onChangePlants}) {
+function NewPlant({user, plants, onChangePlants, types}) {
     const defaultForm = {    
         name:"",
         description:"",
         image_url:""
       }
     const [formData, setFormData]=useState(defaultForm)
-    // const [typeId, setTypeId]=useState(1)
+    const [typeId, setTypeId]=useState(1)
     const [errors, setErrors] = useState([])
     const navigate = useNavigate();
 
@@ -35,7 +35,7 @@ function NewPlant({user, plants, onChangePlants}) {
         setFormData({
           ...formData,
           [key]:value,
-          // type_id:typeId
+          type_id:typeId
         })
     }
 
@@ -62,7 +62,7 @@ function NewPlant({user, plants, onChangePlants}) {
     !user?<h1>Please log in to post your plant!</h1>:
     <div style={style} >
       <h3>Share some plant information</h3>
-      {/* <Filter onChangeId={setTypeId} types={types}/> */}
+      <Filter onChangeId={setTypeId} types={types}/>
       <form className="NewItem" onSubmit={handleSubmit} style={style}>
         <label>name:</label>
         <input type="text" name="name" value={formData.name} onChange={handleChange}/>
